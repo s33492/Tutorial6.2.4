9ec1875f3717968385214108553a21e764b84dee
s33492 <s33492@pjwstk.edu.pl>
2025-04-21 18:18:56 +0200

    Update Feline.java

--- src/Feline.java
+++ src/Feline.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
-public class Feline {
+class Feline extends Animal {
+    private String name;
+    private String noise;
+    public Feline(String name, String noise) {
+        this.name = name;
+        this.noise = noise;
+    }
+    @Override
+    public void makeNoise() {
+        System.out.println(name + " " + noise );
+    }
+    @Override
+    public void roam() {
+        System.out.println("Felines are roaming");
+    }
 }
