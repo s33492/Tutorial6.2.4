869e730d5b0d40eaf39cbfba63210f6deabb0d65
s33492 <s33492@pjwstk.edu.pl>
2025-04-21 18:19:08 +0200

    Update Canine.java

--- src/Canine.java
+++ src/Canine.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
-public class Canine {
+class Canine extends Animal {
+    private String name;
+    private String noise;
+    public Canine(String name, String noise) {
+        this.name = name;
+        this.noise = noise;
+    }
+    @Override
+    public void makeNoise() {
+        System.out.println(name + " " + noise );
+    }
+    @Override
+    public void roam() {
+        System.out.println( "Canines are roaming");
+    }
 }
