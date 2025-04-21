74f62d8dfc49040f4094c2f4ef32a2caf7309ac2
s33492 <s33492@pjwstk.edu.pl>
2025-04-21 18:18:27 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Animal animal = new Animal();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Feline Lion = new Feline("Lion", "roars");
+        Feline Cat = new Feline("Cat", "purrs");
+        Canine Wolf = new Canine("Wolf", "howls");
+        Canine Dog = new Canine("Dog", "barks");
+
+        animal.roam();
+        animal.makeNoise();
+        animal.roam();
+        System.out.println();
+
+        Cat.sleep();
+        Cat.makeNoise();
+        Cat.roam();
+        Lion.makeNoise();
+        Lion.roam();
+        System.out.println();
+
+        Wolf.sleep();
+        Wolf.makeNoise();
+        Wolf.roam();
+        Dog.makeNoise();
+        Dog.roam();
     }
 }
