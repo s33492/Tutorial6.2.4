a7cc25b3c60ae78385240103e385b31f5bb91234
s33492 <s33492@pjwstk.edu.pl>
2025-04-21 18:18:44 +0200

    Update Animal.java

--- src/Animal.java
+++ src/Animal.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class Animal {
+
+
+    public void sleep() {
+            System.out.println( "Animals are sleeping");
+    }
+    public void makeNoise() {
+            System.out.println("animals are making noise " );
+    }
+    public void roam() {
+            System.out.println("animals are roaming");
+    }
 }
+
